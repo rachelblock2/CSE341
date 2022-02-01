bcb2d0b1f13796a22e0675f05bc40bf0e7037834
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-02-01 11:31:29 -0700

    switched location of repo to shop app

--- app.js
+++ app.js
@@ -1,44 +1,77 @@
-// const http = require('http');
-
 const path = require('path');
+const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+// // "start": "nodemon app.js",
+// "start": "node index.js",
+//     "start:dev": "nodemon --watch"
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// app.use((req, res, next) => {
-//     console.log('In the middleware!');
-//     next();
-// });
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const corsOptions = {
+  origin: "https://rachelcse341nodejs.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://rachellof:G78bRrKWgPS7iZZ@cluster0.z8i5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-});
+app.use(errorController.get404);
 
-// const routes = require('./routes');
-
-// function reqListener(req, res) {
-
-// }
-
-// http.createServer(reqListener);
-
-// const server = http.createServer(app); //routes
-// // const server = http.createServer((req, res) => {
-// //     // console.log(req.url, req.method, req.headers);    
-// // });
-
-
-// server.listen(3000);
-
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(
+    MONGODB_URL, options
+  ).then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  
+  .catch(err => {
+    console.log(err);
+  });
