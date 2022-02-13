abd4b05dc58a7166116e7cb640d84979b96c55f4
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-02-12 18:49:46 -0700

    week 6 changes

--- app.js
+++ app.js
@@ -48,11 +48,16 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  .then(user => {
+    if (!user) {
+      return next();
+    }
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    next(new Error(err));
+  });
 });
 
 app.use((req, res, next) => {
@@ -65,7 +70,19 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 const options = {
   useUnifiedTopology: true,
